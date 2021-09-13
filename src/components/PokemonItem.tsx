0cdf2c83fd9002e6f78f3a5e6768f3b5fd0ac158
Bruna Barrón <barron.bruna@gmail.com>
2021-09-13 18:42:04 -0300

    feat: props for PokemonItem

--- src/components/PokemonItem.tsx
+++ src/components/PokemonItem.tsx
@@ -1,7 +1,14 @@
-function PokemonItem() {
+interface IProps {
+  name?: string;
+  // se for opcional, só usar name?
+  //pode ser type = IProps ao invés de interface
+}
+
+function PokemonItem({ name }: IProps) {
   return (
     <div>
-      <h2>Pokemon X</h2>
+      <h2>{name}</h2>
+      {/* se for opcional -> {name || "Pokemon sem nome"} */}
     </div>
   );
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <div>
       <h1>Meu site com react!</h1>
-      <PokemonItem />
+      <PokemonItem name="pikachu" />
+      <PokemonItem name="charmander" />
+      <PokemonItem name="bulbassauro" />
+      <PokemonItem name="dito" />
+      <PokemonItem name="delibird" />
     </div>
   );
 }
