00d08c8700b741528128084ced6c69d6abaf018c
Bruna Barrón <barron.bruna@gmail.com>
2021-09-15 20:01:29 -0300

    feat: styles for pokemon home page

--- src/components/PokemonItem/index.tsx
+++ src/components/PokemonItem/index.tsx
@@ -1,12 +1,14 @@
+import styles from "./styles.module.css";
+
 interface IProps {
   name?: string;
 }
 
 const PokemonItem: React.FC<IProps> = ({ name }) => {
   return (
-    <>
-      <h2>{name}</h2>
-    </>
+    <div className={styles.background}>
+      <h2 className={styles.name}>{name}</h2>
+    </div>
   );
 };
 
