98e3a3a87a43689180d24de5dc7cf4e38f7cd98b
Bruna Barrón <barron.bruna@gmail.com>
2021-09-15 20:26:56 -0300

    feat: install axios and create api with baseUrl

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,11 @@
+import api from "../../services/api";
+
 import PokemonItem from "../../components/PokemonItem";
 import PokemonLogo from "../../assets/images/pokemon.png";
 import styles from "./styles.module.css";
 
 function Home() {
+  api.get("pokemon");
   return (
     <div className={styles.container}>
       <img src={PokemonLogo} alt="Pokemon Logo" className={styles.logo} />
