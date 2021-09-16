7c7d4c37255418294fc5b93452f3234d274d669d
Bruna Barrón <barron.bruna@gmail.com>
2021-09-15 21:59:11 -0300

    feat: create  interfaces for pokemon and api response

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,11 +5,25 @@ import PokemonItem from "../../components/PokemonItem";
 import PokemonLogo from "../../assets/images/pokemon.png";
 import styles from "./styles.module.css";
 
+interface IPokemons {
+  name: string;
+  url: string;
+}
+
+interface IResponse {
+  //resultado do backend
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: IPokemons[];
+}
+
 function Home() {
-  const [pokemons, setPokemons] = useState(null);
+  const [pokemons, setPokemons] = useState<IPokemons[]>();
 
   const getAllPokemons = async () => {
-    const { data } = await api.get("pokemon");
+    const { data } = await api.get<IResponse>("pokemon");
+
     setPokemons(data.results);
   };
 
