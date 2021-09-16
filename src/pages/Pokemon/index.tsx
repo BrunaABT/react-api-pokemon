db3cb0ea211e9da5d7c85365284cd4bf5729d14a
Bruna Barrón <barron.bruna@gmail.com>
2021-09-16 20:18:50 -0300

    feat(Pokemon): create IPokemon Interface

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -3,6 +3,15 @@ import { useParams } from "react-router-dom";
 import api from "../../services/api";
 import styles from "./styles.module.css";
 
+interface IPokemon {
+  abilites: { ability: { name: string } }[];
+  height: number;
+  name: string;
+  weight: number;
+  types: { type: { name: string } }[];
+  sprites: { other: { dream_world: { front_default: string } } };
+}
+
 interface IParams {
   pokemon: string;
 }
@@ -12,7 +21,7 @@ const Pokemon: React.FC = () => {
   const { pokemon: pokemonName } = useParams<IParams>();
 
   const getPokemonInfo = async () => {
-    const { data } = await api.get(`pokemon/${pokemonName}`);
+    const { data } = await api.get<IPokemon>(`pokemon/${pokemonName}`);
 
     setPokemon(data);
   };
